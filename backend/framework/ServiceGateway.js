32702441ba155ef672da8153dea8b817dc8a3bd7
Gang Wei <gwei@pier88health.com>
2018-02-01 22:01:16 -0600

    parse

--- backend/framework/ServiceGateway.js
+++ backend/framework/ServiceGateway.js
@@ -3,7 +3,8 @@
 
 function processRequest(req, res) {
     'user strict';
-    console.log(req.query);
+    console.log('query', req.query);
+    console.log('body', req.body);
     console.log(req.params.ServiceName);
     console.log(req.params.MethodName);
     res.send(req.query);
--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 /*jslint node:true es6:true*/
 let express = require('express'),
     app = express(),
+    cookieParser = require('cookie-parser'),
+    async = require('async'),
+    bodyParser = require('body-parser'),
+    bodyParserUrl = bodyParser.urlencoded({
+        extended: true,
+        keepExtensions: true,
+        limit: '5mb'
+    }),
+    middleWares = [
+        cookieParser(),
+        bodyParser.json({
+            limit: '5mb'
+        }),
+        bodyParserUrl
+    ],
     serviceGateway = require('./backend/framework/ServiceGateway.js');
 
+function parallel(middlewares) {
+    'use strict';
+    return function (req, res, next) {
+        async.each(middlewares, function (mw, cb) {
+            mw(req, res, cb);
+        }, next);
+    };
+}
 
-
+app.use(parallel(middleWares));
 app.get('/svc/:ServiceName/:MethodName', serviceGateway.ProcessRequest);
 app.post('/svc/:ServiceName/:MethodName', serviceGateway.ProcessRequest);
 
