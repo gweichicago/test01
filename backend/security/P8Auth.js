7f816408c73df377679a767d6ee4b960e417cfb2
Gang Wei <gwei@pier88health.com>
2018-02-02 16:19:49 -0600

    anonymous

--- backend/security/P8Auth.js
+++ backend/security/P8Auth.js
@@ -1,10 +1,17 @@
 /*jslint node:true es6:true*/
-
+let services = require('./Services.js'),
+    AuthorizationLevel = require('../enums/AuthorizationLevel.js');
 function authRequest(params, callback) {
     'user strict';
+    let service = services[params.ServiceName];
+    if (!service || !service.AuthorizeLevel) {
+        return callback("Invalid service name or missing security setting");
+    }
+    if (service.AuthorizeLevel === AuthorizationLevel.Anonymous) {
+        return callback(null, "No auth needed.");
+    }
     console.log(params);
     callback(null, "this is authenticated and authorized");
-
 }
 
 
