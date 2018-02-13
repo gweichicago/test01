4aeeb63bc070104b43d39505b0009024f42b85f5
Gary Wei <gary@highground.com>
2018-02-13 00:26:45 -0600

    use async poc

--- backend/framework/ServiceGateway.js
+++ backend/framework/ServiceGateway.js
@@ -1,25 +1,37 @@
 /*jslint node:true es6:true*/
 let p8Auth = require('../security/P8Auth.js'),
     services = require('./WebServices.js');
-function processRequest(req, res) {
+async function processRequest(req, res) {
     'user strict';
-    p8Auth.AuthRequest({
+    let currentuser = await p8Auth.AuthRequest({
         ServiceName: req.params.ServiceName,
         MethodName: req.params.MethodName
-    }, function (error, currentuser) {
-        if (error) {
-            console.log(error);
-            return res.send(error);
-        }
-        if (services[req.params.ServiceName] && services[req.params.ServiceName][req.params.MethodName]) {
-            return services[req.params.ServiceName][req.params.MethodName]({
-                currentuser: currentuser,
-                req: req,
-                res: res
-            });
-        }
-        res.send("Invalid ServiceName or MethodName");
     });
+    if (services[req.params.ServiceName] && services[req.params.ServiceName][req.params.MethodName]) {
+        return services[req.params.ServiceName][req.params.MethodName]({
+            currentuser: currentuser,
+            req: req,
+            res: res
+        });
+    }
+    res.send("Invalid ServiceName or MethodName");
+    // p8Auth.AuthRequest({
+    //     ServiceName: req.params.ServiceName,
+    //     MethodName: req.params.MethodName
+    // }, function (error, currentuser) {
+    //     if (error) {
+    //         console.log(error);
+    //         return res.send(error);
+    //     }
+    //     if (services[req.params.ServiceName] && services[req.params.ServiceName][req.params.MethodName]) {
+    //         return services[req.params.ServiceName][req.params.MethodName]({
+    //             currentuser: currentuser,
+    //             req: req,
+    //             res: res
+    //         });
+    //     }
+    //     res.send("Invalid ServiceName or MethodName");
+    // });
 }
 
 module.exports = {
--- backend/security/P8Auth.js
+++ backend/security/P8Auth.js
@@ -2,30 +2,30 @@
 let services = require('./Services.js'),
     AuthorizationLevel = require('../enums/AuthorizationLevel.js');
 
-function authUserRequest(params, callback) {
+function authUserRequest(params) {
     'user strict';
     console.log('authUserRequest:', params);
-    callback(null, "User leve auth, place holder");
+    return "User leve auth, place holder";
 }
-function authRequest(params, callback) {
+function authRequest(params) {
     'user strict';
     let service = services[params.ServiceName];
     if (!service || !service.AuthorizeLevel) {
-        return callback("Invalid service name or missing security setting");
+        throw "Invalid service name or missing security setting";
     }
     if (service.AuthorizeLevel === AuthorizationLevel.Anonymous) {
-        return callback(null, "No auth needed.");
+        return "No auth needed.";
     }
     if (service.AuthorizeLevel === AuthorizationLevel.Client) {
-        return callback("Not supported right now, use client key for API call");
+        return "Not supported right now, use client key for API call";
     }
     if (service.AuthorizeLevel === AuthorizationLevel.ClientIP) {
-        return callback("Not supported right now, use certain IP address to auth");
+        return "Not supported right now, use certain IP address to auth";
     }
     if (service.AuthorizeLevel === AuthorizationLevel.User) {
-        return authUserRequest(params, callback);
+        return authUserRequest(params);
     }
-    callback("Unsupported auth type!");
+    throw "Unsupported auth type!";
 }
 
 
